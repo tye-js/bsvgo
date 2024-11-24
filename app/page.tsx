910873ac46b1c21e8f39e38ff19d1660119a8b2b
tye-dev <tianye0020@gmail.com>
2024-11-24 22:28:55 +0800

    add some thing in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 
 
 export default function Home() {
-  return <div>Coming Soon</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center text-2xl text-blue-600">make bsv go! go! go!</div>
+    </div>
+  );
 }
